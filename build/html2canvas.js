363bd2a179fc6be2c1b52193a23de582248a2de7
Ariel Yaar <arielyaar@gmail.com>
2019-05-17 15:19:30 -0700

    removed addPseudoElements

--- build/html2canvas.js
+++ build/html2canvas.js
@@ -1061,7 +1061,7 @@ _html2canvas.Parse = function (images, options, cb) {
       stack = renderElement(element, null, false, transparentBackground);
 
     // create pseudo elements in a single pass to prevent synchronous layouts
-    addPseudoElements(element);
+    //addPseudoElements(element);
 
     parseChildren(element, stack, function() {
       if (transparentBackground) {
